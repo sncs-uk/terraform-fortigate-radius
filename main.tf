33bc82aa817b8ff0c1c296130cc9c162918b3b5c
Richard Franks <richard.franks@securenetcoms.co.uk>
2025-11-24 14:22:43 +0000

    Added version constraint, terraform docs

--- main.tf
+++ main.tf
@@ -4,62 +4,64 @@
  * This terraform module configures the RADIUS servers and groups on a FortiGate appliance
  */
 terraform {
+  required_version = ">= 1.13.0"
   required_providers {
     fortios = {
       source  = "fortinetdev/fortios"
+      version = ">= 1.22.0"
     }
   }
 }
 
 locals {
-  vdom_radius_yaml      = {
+  vdom_radius_yaml = {
     for vdom in var.vdoms : vdom => yamldecode(file("${var.config_path}/${vdom}/radius.yaml")) if fileexists("${var.config_path}/${vdom}/radius.yaml")
   }
-  radius_servers_yaml   = { for vdom in var.vdoms : vdom => try(local.vdom_radius_yaml[vdom].radius_servers, []) }
-  radius_groups_yaml    = { for vdom in var.vdoms : vdom => try(local.vdom_radius_yaml[vdom].radius_groups, []) }
+  radius_servers_yaml = { for vdom in var.vdoms : vdom => try(local.vdom_radius_yaml[vdom].radius_servers, []) }
+  radius_groups_yaml  = { for vdom in var.vdoms : vdom => try(local.vdom_radius_yaml[vdom].radius_groups, []) }
 
-  radius_servers        = flatten([
+  radius_servers = flatten([
     for vdom in var.vdoms : [
-      for name, server in local.radius_servers_yaml[vdom] : [ merge(server, { vdom = vdom, name = name }) ]
+      for name, server in local.radius_servers_yaml[vdom] : [merge(server, { vdom = vdom, name = name })]
     ]
   ])
-  radius_groups         = flatten([
+  radius_groups = flatten([
     for vdom in var.vdoms : [
-      for name, group in local.radius_groups_yaml[vdom] : [ merge(group, { vdom = vdom, name = name }) ]
+      for name, group in local.radius_groups_yaml[vdom] : [merge(group, { vdom = vdom, name = name })]
     ]
   ])
 }
 
-resource fortios_user_radius radius {
-  for_each            = { for radius_server in local.radius_servers : radius_server.name => radius_server }
+resource "fortios_user_radius" "radius" {
+  for_each = { for radius_server in local.radius_servers : radius_server.name => radius_server }
 
-  name                = each.value.name
-  server              = each.value.primary.ip
-  secret              = each.value.primary.secret
+  name   = each.value.name
+  server = each.value.primary.ip
+  secret = each.value.primary.secret
 
-  secondary_server    = try(each.value.secondary.ip, null)
-  secondary_secret    = try(each.value.secondary.secret, null)
+  secondary_server = try(each.value.secondary.ip, null)
+  secondary_secret = try(each.value.secondary.secret, null)
 
-  tertiary_server     = try(each.value.tertiary.ip, null)
-  tertiary_secret     = try(each.value.tertiary.secret, null)
+  tertiary_server = try(each.value.tertiary.ip, null)
+  tertiary_secret = try(each.value.tertiary.secret, null)
 
   timeout             = try(each.value.timeout, null)
   source_ip_interface = try(each.value.source_ip_interface, null)
   source_ip           = try(each.value.source_ip, null)
 
-  vdomparam           = each.value.vdom
+  vdomparam = each.value.vdom
 }
 
-resource fortios_user_group groups {
-  depends_on          = [ fortios_user_radius.radius ]
-  for_each            = { for radius_group in local.radius_groups : radius_group.name => radius_group }
+resource "fortios_user_group" "groups" {
+  depends_on = [fortios_user_radius.radius]
+  for_each   = { for radius_group in local.radius_groups : radius_group.name => radius_group }
 
-  name                = each.value.name
+  name = each.value.name
 
-  dynamic member {
-    for_each          = { for member in each.value.members : member => member }
+  dynamic "member" {
+    for_each = { for member in each.value.members : member => member }
     content {
-      name            = member.value
+      name = member.value
     }
   }
 }
